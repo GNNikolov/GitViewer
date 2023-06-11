a09801d73ddffa6114697e34e73f93b813a353fe
Joro <georgenikolaevnikolov@gmail.com>
2023-06-12 00:49:08 +0300

    Created loading UI (shimmer placeholder) when fetching last commit data

---
+++ app/src/main/java/com/gnnikolov/gitviewer/ui/ShimmerLoadingUi.kt
@@ -0,0 +1,53 @@
+package com.gnnikolov.gitviewer.ui
+
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntSize
+
+@Composable
+fun ShimmerLoadingUi(
+    isLoading: Boolean,
+    loadingContent: @Composable () -> Unit,
+    content: @Composable () -> Unit
+) {
+    if (isLoading) {
+        loadingContent()
+    } else {
+        content()
+    }
+}
+
+fun Modifier.shimmerEffect(): Modifier = composed {
+    var size by remember { mutableStateOf(IntSize.Zero) }
+    val transition = rememberInfiniteTransition()
+    val startOffsetX by transition.animateFloat(
+        initialValue = 0f,
+        targetValue =  size.width.toFloat(),
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000)
+        )
+    )
+    background(
+        brush = Brush.linearGradient(
+            colors = listOf(
+                Color(0xFFB8B5B5),
+                Color(0xFF8F8B8B),
+                Color(0xFFB8B5B5)
+            ),
+            start = Offset(startOffsetX, 0f),
+            end = Offset(startOffsetX + size.width.toFloat(), size.height.toFloat())
+        )
+    ).onGloballyPositioned {
+        size = it.size
+    }
+}
--- app/src/main/java/com/gnnikolov/gitviewer/ui/list/RepositoryList.kt
+++ app/src/main/java/com/gnnikolov/gitviewer/ui/list/RepositoryList.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,6 +14,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.gnnikolov.gitviewer.ui.ShimmerLoadingUi
+import com.gnnikolov.gitviewer.ui.shimmerEffect
 
 @Composable
 fun RepositoryList() {
@@ -31,7 +33,7 @@ private fun RepositoryListItem() {
         modifier = Modifier
             .padding(all = 8.dp)
             .fillMaxWidth()
-            .wrapContentWidth()
+            .wrapContentHeight()
     ) {
         Column(
             Modifier
@@ -39,13 +41,11 @@ private fun RepositoryListItem() {
                 .fillMaxWidth()
                 .wrapContentHeight()
         ) {
-            Row(Modifier.padding(top = 8.dp, start = 14.dp)) {
+            Row(Modifier.padding(top = 8.dp, start = 16.dp)) {
                 Icon(
                     tint = Color.Yellow,
                     imageVector = Icons.Filled.Star,
-                    modifier = Modifier
-                        .size(20.dp)
-                        .align(CenterVertically),
+                    modifier = Modifier.align(CenterVertically),
                     contentDescription = null
                 )
                 Text(
@@ -85,51 +85,78 @@ private fun RepositoryListItem() {
                     .padding(horizontal = 16.dp)
                     .padding(top = 24.dp, bottom = 24.dp)
             )
-            Row {
-                Text(
-                    text = "Merge pull request #599 from android/jakew/closing-time/2018-07-24\\n\\nDirect contributors to AOSP",
-                    modifier = Modifier
-                        .align(CenterVertically)
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    style = MaterialTheme.typography.subtitle1,
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis
-                )
-            }
-            Text(
-                text = "Change author: go6o",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 8.dp),
-                color = Color(0xFF8A8A8A),
-                style = MaterialTheme.typography.subtitle2,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                text = "Change date: 2018-11-13 20:14:09",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 4.dp),
-                color = Color(0xFF8A8A8A),
-                style = MaterialTheme.typography.subtitle2,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                text = "Commit id: 7eb394dc7d489370de1e32bb91755a24bbd75627",
-                modifier = Modifier
-                    .wrapContentWidth()
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 4.dp)
-                    .padding(bottom = 16.dp),
-                color = Color(0xFF8A8A8A),
-                style = MaterialTheme.typography.subtitle2,
-                maxLines = 2
-            )
+            ShimmerLoadingUi(
+                isLoading = true,
+                content = { LastCommitListItemContent() },
+                loadingContent = { LastCommitListItemLoading() })
         }
+    }
+}
+
+@Composable
+private fun LastCommitListItemLoading() {
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(32.dp)
+                .padding(horizontal = 16.dp)
+                .shimmerEffect()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(16.dp)
+                .padding(horizontal = 16.dp)
+                .shimmerEffect()
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(16.dp)
+                .padding(horizontal = 16.dp)
+                .shimmerEffect(),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Composable
+private fun LastCommitListItemContent() {
+    Column {
+        Text(
+            text = "Merge pull request #599 from android/jakew/closing-time/2018-07-24\\n\\nDirect contributors to AOSP",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            style = MaterialTheme.typography.subtitle1,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Medium
+        )
+        Text(
+            text = "Change by GitUser at 2018-11-13 20:14:09",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .padding(top = 8.dp),
+            color = Color(0xFF8A8A8A),
+            style = MaterialTheme.typography.subtitle2,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = "Id: 7eb394dc7d489370de1e32bb91755a24bbd75627",
+            modifier = Modifier
+                .wrapContentWidth()
+                .padding(horizontal = 16.dp)
+                .padding(top = 4.dp)
+                .padding(bottom = 16.dp),
+            color = Color(0xFF8A8A8A),
+            style = MaterialTheme.typography.subtitle2,
+            maxLines = 2
+        )
     }
 }
