80283b2a602f035a3b5f996f7647015645abf62c
Joro <georgenikolaevnikolov@gmail.com>
2023-06-15 00:15:19 +0300

    Dependencies and compose compiler versions update.

--- app/src/main/java/com/gnnikolov/gitviewer/data/remote/GitHubApi.kt
+++ app/src/main/java/com/gnnikolov/gitviewer/data/remote/GitHubApi.kt
@@ -9,10 +9,12 @@ import retrofit2.Retrofit
 object GitHubApi {
     private const val URL = "https://api.github.com"
 
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
     private val retrofit = Retrofit.Builder()
-        .addConverterFactory(Json {
-            ignoreUnknownKeys = true
-        }.asConverterFactory("application/json".toMediaType()))
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(URL).build()
 
     val gitRepoService by lazy { retrofit.create(GitRepoService::class.java) }
