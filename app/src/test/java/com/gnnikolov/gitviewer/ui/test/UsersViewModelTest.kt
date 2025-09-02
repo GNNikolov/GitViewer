b703ab86f36cb1ab94fc73956766ec3f04518240
Joro <georgenikolaevnikolov@gmail.com>
2025-09-03 01:16:53 +0300

    [Test] Add unit test for validating Loading and Error states in UsersViewModel.kt

--- app/src/test/java/com/gnnikolov/gitviewer/ui/test/UsersViewModelTest.kt
+++ app/src/test/java/com/gnnikolov/gitviewer/ui/test/UsersViewModelTest.kt
@@ -13,13 +13,12 @@ import io.mockk.impl.annotations.MockK
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
-import org.junit.Assert
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
@@ -31,12 +30,11 @@ class UsersViewModelTest {
 
     private lateinit var instance: UsersViewModel
 
-    private val testDispatcher = StandardTestDispatcher()
-
     @Before
     fun setup() {
         MockKAnnotations.init(this)
         Dispatchers.setMain(StandardTestDispatcher())
+        instance = UsersViewModel(repository)
     }
 
     @Test
@@ -50,14 +48,22 @@ class UsersViewModelTest {
         )
         val mockUsers = Users(mockData)
         coEvery { repository.getUsers() } returns mockUsers.value
-        instance = UsersViewModel(repository,)
         instance.uiState.test {
-            testDispatcher.scheduler.advanceUntilIdle()
             assertEquals(Async.Loading, awaitItem())
-            testDispatcher.scheduler.advanceUntilIdle()
             val successState = awaitItem()
-            Assert.assertTrue("State should be Success", successState is Async.Success)
+            assertTrue("State should be Success", successState is Async.Success)
             assertEquals(mockUsers, (successState as Async.Success).data)
+            cancelAndConsumeRemainingEvents()
+        }
+        coVerify(exactly = 1) { repository.getUsers() }
+    }
+
+    @Test
+    fun `uiState emits Loading and Error state`() = runTest {
+        coEvery { repository.getUsers() } returns emptyList()
+        instance.uiState.test {
+            assertEquals(Async.Loading, awaitItem())
+            assertTrue(awaitItem() is Async.Error)
             cancelAndConsumeRemainingEvents()
         }
         coVerify(exactly = 1) { repository.getUsers() }
